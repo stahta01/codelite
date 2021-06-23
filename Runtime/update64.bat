4eae9ced5c0d53b47959e22792cbd4564123d428
Tim Stahlhut <stahta01@gmail.com>
2021-06-23 15:00:10 -0400

    build: Edit "Runtime/update64.bat"

--- Runtime/update64.bat
+++ Runtime/update64.bat
@@ -7,9 +7,11 @@ set BIN_DIR=%CODELITE_SRC_DIR%\build-Win_x64_Release\bin
 set LIB_DIR=%CODELITE_SRC_DIR%\build-Win_x64_Release\lib
 set SDK_DIR==%CODELITE_SRC_DIR%\sdk
 set RUNTIME_DIR=%CODELITE_SRC_DIR%\Runtime
-set WXWIN=C:\src\wxWidgets
++set COMPILER_DIR=C:\GreenApps64\x86_64-7.1.0-release-posix-seh-rt_v5-rev2\mingw64
++set WXWIN=C:\repos\git\wxWidgets
++set WXSUFFIX=u
 
-::IF EXIST D:\software\NUL (set TARGET_DIR=D:\software)
+IF EXIST %HOMEDRIVE%\software\ (set TARGET_DIR=%HOMEDRIVE%\software)
 IF EXIST ..\wxcrafter\wxcrafter.accelerators (set COPY_WXC_RESOURCES=1)
 
 xcopy %RUNTIME_DIR%\config\*.default "%TARGET_DIR%\CodeLite\config\" /E /I /H /Y /EXCLUDE:excludes
@@ -41,8 +43,8 @@ IF EXIST %RUNTIME_DIR%\PHP.zip ( copy PHP.zip "%TARGET_DIR%\CodeLite\" )
 xcopy %LIB_DIR%\*.dll "%TARGET_DIR%\CodeLite\plugins\" /E /I /H /Y /EXCLUDE:excludes
 
 if "%WXWIN%" == "" GOTO OTHERS
-xcopy %WXWIN%\lib\gcc_dll\wxmsw*u_*gcc_cl.dll "%TARGET_DIR%\CodeLite\" /E /I /H /Y /EXCLUDE:excludes
-xcopy %WXWIN%\lib\gcc_dll\wxbase*u_*gcc_cl.dll "%TARGET_DIR%\CodeLite\" /E /I /H /Y /EXCLUDE:excludes
+xcopy %WXWIN%\lib\gcc_dll\wxmsw*%WXSUFFIX%_*gcc_cl.dll "%TARGET_DIR%\CodeLite\" /E /I /H /Y /EXCLUDE:excludes
+xcopy %WXWIN%\lib\gcc_dll\wxbase*%WXSUFFIX%_*gcc_cl.dll "%TARGET_DIR%\CodeLite\" /E /I /H /Y /EXCLUDE:excludes
 :: xcopy %WXWIN%\lib\gcc_dll\wxrc.exe "%TARGET_DIR%\CodeLite\" /E /I /H /Y /EXCLUDE:excludes
 
 if "%COPY_WXC_RESOURCES%" == "1" (copy ..\wxcrafter\wxcrafter.accelerators  "%TARGET_DIR%\CodeLite\plugins\resources" )
@@ -52,10 +54,11 @@ if "%COPY_WXC_RESOURCES%" == "1" (copy ..\wxcrafter\wxcrafter.accelerators  "%TA
 copy %RUNTIME_DIR%\codelite_indexer.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\codelite-cc.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\codelite_cppcheck.exe "%TARGET_DIR%\CodeLite\" /Y
-copy %RUNTIME_DIR%\codelite_launcher.exe "%TARGET_DIR%\CodeLite\" /Y
+::copy %RUNTIME_DIR%\codelite_launcher.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\codelite-echo.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %SDK_DIR%\clang\lib\clang-format-64.exe "%TARGET_DIR%\CodeLite\codelite-clang-format.exe" /Y
 copy %SDK_DIR%\libssh\lib\libssh.dll "%TARGET_DIR%\CodeLite\libssh.dll" /Y
+copy %SDK_DIR%\libssh\lib\libzlib.dll "%TARGET_DIR%\CodeLite\libzlib.dll" /Y
 copy %RUNTIME_DIR%\codelite-exec.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\codelite.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\codelite-make.exe "%TARGET_DIR%\CodeLite\" /Y
@@ -65,6 +68,9 @@ copy %RUNTIME_DIR%\astyle.sample "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\php.sample "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\pthreadGC2.dll "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\wx-config.exe "%TARGET_DIR%\CodeLite\" /Y
+copy %COMPILER_DIR%\bin\libgcc_s_seh-1.dll "%TARGET_DIR%\CodeLite\" /Y
+copy %COMPILER_DIR%\bin\libwinpthread-1.dll "%TARGET_DIR%\CodeLite\" /Y
+copy "%COMPILER_DIR%\bin\libstdc++-6.dll" "%TARGET_DIR%\CodeLite\" /Y
 
 :END
 
