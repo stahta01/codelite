3b4d68d9fec1af0c3af594d5f04e2c74cc10eb51
Tim Stahlhut <stahta01@gmail.com>
2021-06-23 23:46:43 -0400

    Edit zlib DLL filename in Runtime/update64.bat

--- Runtime/update64.bat
+++ Runtime/update64.bat
@@ -58,7 +58,7 @@ copy %RUNTIME_DIR%\codelite_cppcheck.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\codelite-echo.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %SDK_DIR%\clang\lib\clang-format-64.exe "%TARGET_DIR%\CodeLite\codelite-clang-format.exe" /Y
 copy %SDK_DIR%\libssh\lib\libssh.dll "%TARGET_DIR%\CodeLite\libssh.dll" /Y
-copy %SDK_DIR%\libssh\lib\libzlib.dll "%TARGET_DIR%\CodeLite\libzlib.dll" /Y
+copy %SDK_DIR%\libssh\lib\libzlib.dll "%TARGET_DIR%\CodeLite\zlib1.dll" /Y
 copy %RUNTIME_DIR%\codelite-exec.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\codelite.exe "%TARGET_DIR%\CodeLite\" /Y
 copy %RUNTIME_DIR%\codelite-make.exe "%TARGET_DIR%\CodeLite\" /Y
